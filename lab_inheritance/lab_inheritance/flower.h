beebff45a6837aeb9208a7f220fbf41378359cab
Max Asselmeier <ma53@illinois.edu>
2020-10-04 20:25:25 -0500

    adding lab_inheritance

---
+++ lab_inheritance/lab_inheritance/flower.h
@@ -0,0 +1,31 @@
+/**
+ * @file flower.h
+ * Declaration of the Flower class.
+ */
+
+#pragma once
+
+#include "shape.h"
+#include "rectangle.h"
+#include "circle.h"
+#include "triangle.h"
+#include "vector2.h"
+
+/**
+ * A subclass of Drawable that can draw a flower
+ */
+class Flower : public Drawable
+{
+  private:
+    Vector2 stem_center;
+    Rectangle stem;
+    Circle pistil; // center piece of flower
+    Vector2 leaf_center;  
+    Triangle leaf;
+
+    void drawPetals(cs225::PNG* canvas, const Vector2& center, int x, int y) const;
+
+  public:
+    Flower(const Vector2& center);
+    void draw(cs225::PNG* canvas) const;
+};
